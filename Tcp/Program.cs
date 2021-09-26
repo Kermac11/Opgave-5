e9367a68136c34e68d096b09f4d67c0daaa50205
Kermac11 <73934167+Kermac11@users.noreply.github.com>
2021-09-26 23:42:22 +0200

    Initial commit

---
+++ Tcp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Sockets;
+using ModelLib;
+
+namespace PlainServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server test = new Server();
+            test.Start(new TcpClient());
+        }
+    }
+}
---
+++ Tcp/server.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using ModelLib;
+
+namespace PlainServer
+{
+    public class Server
+    {
+        private static List<FootballPlayer> players = new List<FootballPlayer>()
+        {
+            new FootballPlayer(1,"Peter",80,1),
+            new FootballPlayer(5,"John", 50,5),
+            new FootballPlayer(7,"Jens", 77,7),
+            new FootballPlayer(21,"Knud",150,21)
+        };
+        public void Start(TcpClient socket)
+        {
+            TcpListener listener = new TcpListener(IPAddress.Loopback, 2121);
+            listener.Start();
+            while (true)
+            {
+                socket = listener.AcceptTcpClient();
+                Task.Run(() => Server.DoClient(socket));
+
+            }
+        }
+
+        private static void DoClient(TcpClient socket)
+        {
+
+            using (StreamReader sr = new StreamReader(socket.GetStream()))
+            using (StreamWriter sw = new StreamWriter(socket.GetStream()))
+            {
+                sw.AutoFlush = true;
+                string str = sr.ReadLine();
+                string[] command = str.Split('/');
+                switch (command[0])
+                {
+                    case "HentAlle":
+                        foreach (FootballPlayer player in players)
+                        {
+                         sw.WriteLine(JsonSerializer.Serialize(player));
+                        }
+                        break;
+                    case "Hent":
+                        FootballPlayer place = players.Find(p => p.Id == int.Parse(command[1]));
+                        sw.WriteLine(JsonSerializer.Serialize(place));
+                        break;
+                    case "Gem":
+                        players.Add(JsonSerializer.Deserialize<FootballPlayer>(command[1]));
+                        break;
+                }
+                Console.WriteLine("Server har modtaget besked : " + str);
+                sw.WriteLine(str);
+                sw.Flush();
+            }
+
+            socket?.Close();
+        }
+    }
+}
